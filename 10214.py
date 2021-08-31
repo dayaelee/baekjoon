2c2b847330ff5b030ffdd3a7982cea80d276f04a
Dayae Lee <dayae.dev@gmail.com>
2021-08-31 18:37:37 +0900

    Revert "n = int(input())"
    This reverts commit 07bb83df1264055231c06ca90cd3b7e6d2ae3c78.

--- 10214.py
+++ 10214.py
@@ -1,17 +1,28 @@
 n = int(input())
-if n == 0:
-    quit()
-ysum=0
-ksum=0
-for i in range(n):
-    for i in range(9):
-        a = input().split()
-        ysum += int(a[0])
-        ksum += int(a[1])
+if n==0:
+  quit()
 
-    if ysum > ksum:
-        print('Yonsei')
-    elif ysum < ksum:
-        print('Korea')
+ycount=0
+kcount=0
+
+for i in range(n):
+  for i in range(9):
+    a=input().split()
+    if a[0]>a[1]:
+      ycount+=1
+    elif a[0]<a[1]:
+      kcount+=1
+      print(kcount)
     else:
-        print('Draw')
+      continue
+
+ 
+  if ycount>kcount:
+    print('Yonsei')
+  elif ycount<kcount:
+    print('Korea')
+  else:
+    print('Draw')
+
+  ycount=0
+  kcount=0
