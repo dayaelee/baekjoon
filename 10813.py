c79c2f5571ef8dab9896f441217db1eb3c2739f4
dayaelee <dayae.dev@gmail.com>
2023-09-20 22:04:48 +0900

    [solved] 백준 10813 공 바꾸기

---
+++ 10813.py
@@ -0,0 +1,14 @@
+n, m = map(int, input().split())
+
+basket=[]
+
+for i in range(1, n+1):
+    basket.insert(i, i)
+
+for i in range(0, m):
+    i, j = map(int, input().split())
+    tmp = basket[i-1]
+    basket[i-1] = basket[j-1]
+    basket[j-1] = tmp
+
+print(*basket)
