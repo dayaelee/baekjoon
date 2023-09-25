16e95b0bb15c9ce46b143eeaefb04b9200236a32
dayaelee <dayae.dev@gmail.com>
2023-09-25 19:26:51 +0900

    [solved] 백준 1085 직사각형에서 탈출

---
+++ 1085.py
@@ -0,0 +1,18 @@
+x, y, w, h = map(int, input().split(" "))
+
+num = []
+for i in range(0, 4):
+    num.append(0)
+
+num[0] = h-y
+num[1] = x-0
+num[2] = w-x
+num[3] = y-0
+
+minimum = num[0]
+
+for i in range(0, 4):
+    if minimum >= num[i]:
+        minimum = num[i]
+
+print(minimum)
