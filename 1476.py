55c450acbba2d8b200dbb885e0482b6c1691c313
dayaelee <dayae.dev@gmail.com>
2023-09-28 16:20:43 +0900

    [solved] 백준 1748 수 이어 쓰기 1

---
+++ 1476.py
@@ -0,0 +1,23 @@
+e, s, m = map(int, input().split())
+ee, ss, mm = 0, 0, 0
+count=0
+while 1:
+    if ee == 16:
+        ee = 1
+
+    if ss == 29:
+        ss = 1
+    
+    if mm == 20:
+        mm = 1
+
+
+    if ee == e and ss == s and mm == m:
+        print(count)
+        break
+
+    ee=ee+1
+    ss=ss+1
+    mm=mm+1
+    count=count+1
+   # print(count)
