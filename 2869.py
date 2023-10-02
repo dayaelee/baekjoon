6ba53173d6d1038d988fdc6f098a89b65ce15b22
dayaelee <dayae.dev@gmail.com>
2023-10-02 23:43:32 +0900

    [solved] 2869 달팽이는 올라가고 싶다 - 리뷰필요함

--- 2869.py
+++ 2869.py
@@ -1,37 +1,25 @@
-a, b, v = map(int, input().split())
+# a, b, v = map(int, input().split())
 
-start = 0
-now = 0
-day = 1
+# day=0
+# namuge=0
 
-no_one_night_day = (v//(a-b)) -1
-print("no_one_night_day",no_one_night_day)
-
-values =  (no_one_night_day * (a-b))+b
+# day = (v//(a-b))
+# namuge= (v%(a-b))
+# print("day",day)
 
 
-print("values",values)
-print("     v", v)
-count=0
+# if day>=1 and namuge ==0:
+#     if b==1 and :
+#         print(day)
+#     else:
+#         print(day-b)
 
-if values>=v:
-        print(no_one_night_day)
+# elif day>=1 and namuge !=0 and namuge>b:
+#     print(day+1)
+# elif day>=1 and namuge !=0 and namuge<b:
+#     print(day)
+A, B, V = map(int, input().split())
+if (V-B)%(A-B) == 0:
+    print ((V-B)//(A-B))
 else:
-    while 1:
-        if count==0:
-            no_one_night_day+=1
-            values=values-b
-            count=1
-        else:
-            if values>=v:
-                print(no_one_night_day)
-                break
-            values=values+a 
-            if values>=v:
-                print(no_one_night_day)
-                break
-            no_one_night_day+=1
-            values=values-b
-
-
-        
+    print ((V-B)//(A-B)+1)
