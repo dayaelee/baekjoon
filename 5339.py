efc26161ed03f16e119d51d974270a5969e43c11
dayaelee <dayae.dev@gmail.con>
2024-08-21 20:43:02 +0900

    [solved] 백준 5339 콜센터

--- 5339.py
+++ 5339.py
@@ -1,14 +1,10 @@
-print('SHIP NAME      CLASS          DEPLOYMENT IN SERVICE')
-print('N2 Bomber      Heavy Fighter  Limited    21        ')
-print('J-Type 327     Light Combat   Unlimited  1         ')
-print('NX Cruiser     Medium Fighter Limited    18        ')
-print('N1 Starfighter Medium Fighter Unlimited  25        ')
-print('Royal Cruiser  Light Combat   Limited    4         ')
-
-
-SHIP NAME      CLASS          DEPLOYMENT IN SERVICE
-N2 Bomber      Heavy Fighter  Limited    21        
-J-Type 327     Light Combat   Unlimited  1         
-NX Cruiser     Medium Fighter Limited    18        
-N1 Starfighter Medium Fighter Unlimited  25        
-Royal Cruiser  Light Combat   Limited    4         
+print('     /~\\')
+print('    ( oo|')
+print('    _\=/_')
+print('   /  _  \\')
+print('  //|/.\\|\\\\')
+print(' ||  \\ /  ||')
+print('============')
+print('|          |')
+print('|          |')
+print('|          |')
