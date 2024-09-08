f32d7d1c5caa88500505ec08ff8b39f8af34571a
dayaelee <dayae.dev@gmail.com>
2024-09-09 00:58:17 +0900

    [solved] 백준 1012 유기농 배추

--- 1012.py
+++ 1012.py
@@ -1,42 +1,42 @@
 import sys
-input=sys.stdin.readline
 from collections import deque
+input=sys.stdin.readline
 
 dx= [0, 0, 1, -1]
 dy= [1, -1, 0, 0]
 
-t = int(input())
-for j in range(t):
-    m, n, k = map(int, input().split())
-    mapp = [[0] * m for _ in range(n)]
-    for i in range(k):
-        wm, wn = map(int, input().split())
-        mapp[wn][wm] = 1
-
-    visited = [[False] * m for _ in range(n)]
-
-    list = deque()
-
-    def bfs(y, x, visited):
+answer=[]
+def bfs(y, x):
+        queue = deque()
         visited[y][x] = True
-        list.append([y, x])
+        queue.append([y, x])
 
-        while list:
-            ny, nx = list.popleft()
-            visited[ny][nx]= True
+        while queue:
+            ny, nx = queue.popleft()
             for ii in range(4):
                 yy= ny+dy[ii]
                 xx= nx+dx[ii]
 
                 if 0<=yy<n and 0<=xx<m and mapp[yy][xx]==1 and  visited[yy][xx] == False:
-                    list.append([yy, xx])
+                    visited[yy][xx] = True
+                    queue.append([yy, xx])
 
+t = int(input())
+for j in range(t):
+    m, n, k = map(int, input().split())
+    mapp = [[0] * m for _ in range(n)]
+    visited = [[False] * m for _ in range(n)]
     cnt = 0
-
+    for i in range(k):
+        wm, wn = map(int, input().split())
+        mapp[wn][wm] = 1
+    
     for ix in range(n):
         for jx in range(m):
-            if mapp[ix][jx]==1:
-                if visited[ix][jx] == False:
+            if mapp[ix][jx]==1 and visited[ix][jx] == False:
                     cnt+=1
-                    bfs(ix, jx, visited)
-    print(cnt)
+                    bfs(ix, jx)
+    answer.append(cnt)
+
+for i in answer:
+  print(i)
