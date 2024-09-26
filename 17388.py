2d1b273eeba7b222fd1cfd44d94d1909bddcd995
dayaelee <dayae.dev@gmail.com>
2024-09-26 15:35:41 +0900

    [solved] 백준 17388 와글와글 숭고한

---
+++ 17388.py
@@ -0,0 +1,18 @@
+tmp = list(map(int, input().split()))
+
+if ((tmp[0]+tmp[1]+tmp[2]>=100)):
+    print("OK")
+else:
+    a=min(tmp[0], tmp[1],tmp[2])
+
+    for i in range(3):
+        if a== tmp[i]:
+            if i == 0:
+                print("Soongsil")
+            elif i == 1:
+                print("Korea")
+            elif i == 1:
+                print("Hanyang")
+
+            
+
