032559d1790bf45ceed24905ef5b593782f7ae30
dayaelee <dayae.dev@gmail.com>
2024-10-03 13:59:28 +0900

    [solved] 백준 9663 n-queen

--- 9663.py
+++ 9663.py
@@ -1,72 +1,32 @@
-'''
-N-Queen  N × N인 체스판 위에 퀸 N개를 서로 공격할 수 없게 놓는 문제이다.
-퀸은 상하좌우와 대각선 4방향 모두 끝까지 이동할 수있음 
-각 퀸의 경로에 다른 퀸이 있어서는 안된다. 
-'''
+
+
 n = int(input())
 
-# 이동용
-dy= [1, -1, 0, 1]
-dx= [0, 0, -1, 1]
+mapp= [0 for _ in range(n)]
+cnt=[0]
 
-# 말의 공격 범위(상하좌우+대각선)
-dyy= [1, -1, 0, 0, 1, 1, -1, -1]
-dxx= [0, 0, -1, 1, -1, 1, -1, 1]
-
-mapp = [[0 for _ in range(n)] for _ in range(n)] 
-
-print(mapp)
-
-# 시작을 어디서 부터 하냐에 따라서 갈림 
-# 따라서 전체 순회가 필요 
-stack = []
-visited = [[False for _ in range(n)] for _ in range(n)]
-def solution(y, x, visited, cnt):
+def chess(depth, cnt):
+    if depth == n:
+        cnt[0]+=1
+        return 
+    
     for i in range(n):
-        for j in range(n):
-            stack.append([i, j])
-            mapp[i][j]=1
-            visited[i][j] = True
+        mapp[depth] = i
 
-            for q in range(8):
-                ry = i
-                rx = j
-                for p in range(n):
-                    ry+=dyy[q]
-                    rx+=dxx[q]
-                    if ry <0 or rx <0 or ry>=n or rx>=n:
-                        continue
+        if possible(depth):
+            chess(depth+1, cnt)
 
-                    visited[ry][rx]=True
-
-            print('')
-            for ii in range(n):
-                print('hi',visited[ii])
-            print('!!!!!!!!!!!!!!!!!!!!!!!!!!')
-            visited[i][j] = False
-            
-            for q in range(8):
-                ry = i
-                rx = j
-                for p in range(n):
-                    ry+=dyy[q]
-                    rx+=dxx[q]
-                    if ry <0 or rx <0 or ry>=n or rx>=n:
-                        continue
-
-                    visited[ry][rx]=False
-            
-            print('')
-            for ii in range(n):
-                print('hi',visited[ii])
-            print('???????????????????????????')
-            
-
-            stack.pop()
-
-            print(visited)
-            mapp[i][j]=0
+def possible(col):
+    for i in range(col):
+        # 같은 열에 있는지 
+        if mapp[i] == mapp[col]: # depth 추가하기전 
+            return False
+    
+        # 대각선에 있는지 
+        if abs(mapp[i]-mapp[col]) == abs(i-col):
+            return False
+    return True
 
 
-
-solution(1, 1, visited, 0)
+chess(0, cnt)
+print(cnt[0])
