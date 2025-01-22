f8a40c26ad46f5564e5adc2d7ab1a0208a81a02d
dayaelee <dayae.dev@gmail.com>
2025-01-22 22:09:46 +0900

    [solved] 백준 에디터

---
+++ 1406.py
@@ -0,0 +1,28 @@
+import sys
+from collections import deque 
+input = sys.stdin.readline
+
+startS = input().strip()
+m = int(input()) # 입력할 명령어의 개수
+totalM=[]
+bag=[]
+startS=list(startS)
+rightS=deque([])
+
+for i in range(m):
+    tmp = input().split()
+    if len(tmp)==1:
+        if tmp[0]=='L':
+            if len(startS)!=0:
+                value = startS.pop()
+                rightS.appendleft(value)
+        elif tmp[0] == 'D':
+            if len(rightS)!=0:
+                value = rightS.popleft()
+                startS.append(value)
+        elif tmp[0] =='B':
+            if len(startS)!=0:
+                startS.pop()
+    else:
+        startS.append(tmp[1])
+print(''.join(startS+list(rightS)))
