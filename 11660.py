5e3383161da1bffaab408699be60336e3f85568a
dayaelee <dayae.dev@gmail.com>
2025-01-30 01:40:18 +0900

    [solved] 백준 11660 구간 합 구하기 5

--- 11660.py
+++ 11660.py
@@ -2,20 +2,29 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
+ntotal=[]
 
-arr = []
 for i in range(n):
-    a = list(map(int, input().split()))
-    arr.append(a)
-    
-dp = [[0]*(n+1) for i in range(n+1)]
+    ntotal.append(list(map(int, input().split())))
 
+dp=[[0 for _ in range(n+1)],[0, ntotal[0][0]]]
 for i in range(1, n+1):
-    for j in range(1, n+1):
-        dp[i][j] = dp[i][j-1] + dp[i-1][j] - dp[i-1][j-1] + arr[i-1][j-1]
-        
-for k in range(m):
-    x1,y1,x2,y2 = map(int,input().split())
-    
-    result = dp[x2][y2] - dp[x2][y1-1] -dp[x1-1][y2] + dp[x1-1][y1-1]
-    print(result)
+    if i == 1:
+        for j in range(2, n+1):
+            dp[i].append(dp[i][j-1]+ntotal[i-1][j-1])
+    else:
+        dp.append([0])
+        dp[i].append(dp[i-1][1]+ntotal[i-1][0])
+        for j in range(2, n+1):
+            dp[i].append(dp[i][j-1]+dp[i-1][j]-dp[i-1][j-1]+ntotal[i-1][j-1])
+
+for i in range(m):
+    y1, x1, y2, x2 = map(int, input().split())
+    result=0
+    if x1 == y1 == 1 and x2 == y2 == n:
+        print(dp[n][n])
+    elif x1==x2 and y1==y2:
+        print(ntotal[y2-1][x2-1])
+    else:
+        result = dp[y2][x2]-dp[y2][x1-1]-dp[y1-1][x2]+dp[y1-1][x1-1]
+        print(result)
