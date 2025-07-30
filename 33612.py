0b7bbaf8b5bd23843b4bafe23db90ced82ad363c
dayaelee <dayae.dev@gmail.com>
2025-07-31 00:09:06 +0900

    [solved] baekjoon 33612 피갤컵

--- 33612.py
+++ 33612.py
@@ -4,8 +4,11 @@ initY = 2024
 initM = 8
 
 if n>1:
-    initY += (initM+(7*n))//12
-    M = (initM+(7*n))%12
+    initY += (initM+(7*(n-1)))//12
+    M = (initM+(7*(n-1)))%12
+    if M==0:
+        M=12
+        initY-=1
     print(initY, M)
 elif n==1:
     print(2024, 8)
