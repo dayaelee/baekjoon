58327467f1205a1375e628c36db63e01219f9f50
dayaelee <dayae.dev@gmail.com>
2025-09-02 10:30:31 +0900

    [feat] baekjoon 10448 유레카 이론

--- B_10448.java
+++ B_10448.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -11,40 +10,30 @@ public class Main {
         int[] check = new int[1001];
         for (int i = 0; i <50 ; i++) {
             int tmp = (i*(i+1))/2;
-            if (tmp <= 1000){
+            if (tmp <= 1000)
                 check[i]=tmp;
-            }
         }
 
         int t = Integer.parseInt(str);
         for (int i = 0; i < t; i++) {
             str = br.readLine();
             int n = Integer.parseInt(str);
-            int cnt =44;
             int answer = 0;
-            while (cnt>=0) {
-                int tmp = n;
-                for (int j = cnt; j > 0; j--) {
-                    for (int k = j; k > 0; k--) {
-                        for (int l = k; l > 0; l--) {
-                            tmp = n;
-                            if (tmp-check[j]-check[k]-check[l]==0){
-                                answer =1;
-                                break;
-                            }
-                        }
-                        if (answer == 1){
+            int tmp = n;
+            for (int j = 44; j > 0; j--) {
+                for (int k = j; k > 0; k--) {
+                    for (int l = k; l > 0; l--) {
+                        tmp = n;
+                        if (tmp-check[j]-check[k]-check[l]==0){
+                            answer =1;
                             break;
                         }
                     }
-                    if (answer == 1){
+                    if (answer == 1)
                         break;
-                    }
                 }
-                if (answer == 1){
+                if (answer == 1)
                     break;
-                }
-                cnt--;
             }
             System.out.println(answer);
         }
