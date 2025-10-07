65f1788e3d793de66bfe34feda4012056e7dce86
dayaelee <dayae.dev@gmail.com>
2025-10-07 22:49:41 +0900

    [solved] baekjoon 30328 Java Warriors

---
+++ B_30328.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.SQLOutput;
+import java.util.*;
+
+public class Main {
+    public static boolean flag;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int n = Integer.parseInt(str);
+
+        System.out.println(n*4000);
+
+
+    }
+}
+
