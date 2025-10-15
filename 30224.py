45388e8a282fc027c8c9c84e4ae71de9e7afdceb
dayaelee <dayae.dev@gmail.com>
2025-10-15 22:28:54 +0900

    [solve] 30224 Lucky 7

---
+++ 30224.py
@@ -0,0 +1,16 @@
+
+n = input()
+
+if '7' not in n:
+    if int(n)%7!=0:
+        print(0)
+    else:
+        print(1)
+else:
+    if int(n)%7!=0:
+        print(2)
+    else:
+        print(3)
+        
+
+
