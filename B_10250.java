f4c4da5380b77e0074aec0fd395038457ece7c92
dayaelee <dayae.dev@gmail.com>
2025-10-22 03:45:45 +0900

    [solved] baekjoon 10250 ACM 호텔

---
+++ B_10250.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str = br.readLine();
+
+        int t = Integer.parseInt(str);
+
+        while(t-- > 0){
+            str = br.readLine();
+            StringTokenizer st = new StringTokenizer(str);
+            int h = Integer.parseInt(st.nextToken()); // 층 수
+            int w = Integer.parseInt(st.nextToken()); // 호 수 
+            int n = Integer.parseInt(st.nextToken()); // N번째로 도착한 손님의 호수? 
+
+            int g = n/h; // 열 
+            int last = n%h;
+            String ans = "";
+            if (last>0){
+                String tmp = ""+last;
+                if ((""+g).length()<2){
+                    if(g+1<10){
+                        tmp = tmp+"0";
+                    }
+                }
+                int anstmp = (g+1);
+                ans = ""+tmp+anstmp;
+            }else{
+                String tmp = ""+h;
+                if ((""+g).length()<2){
+                    tmp = tmp+"0";
+                }
+                int anstmp = g;
+                ans = ""+tmp+anstmp;
+            }
+            bw.write(ans+"\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
