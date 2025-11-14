58cf21afd3b09ffb0da66e731d2177d871d5f220
dayaelee <dayae.dev@gmail.com>
2025-11-15 01:08:55 +0900

    [solved] beakjoon 34411 M-Climb

---
+++ B_34411.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int n = Integer.parseInt(str);
+
+        str = br.readLine();
+        int m = Integer.parseInt(str);
+
+        str = br.readLine();
+        int nn = Integer.parseInt(str);
+        
+        System.out.println(n*m);
+        
+    }
+}
