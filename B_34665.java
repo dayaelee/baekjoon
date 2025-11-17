dca20c09bd3e41480d23e2b18b6591c7ad9fca20
dayaelee <dayae.dev@gmail.com>
2025-11-17 22:48:00 +0900

    [solved] baekjoon 34665 가희와 교통 요금

---
+++ B_34665.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        String str1 = br.readLine();
+
+        if (str.equals(str1)){
+            System.out.println("0");
+        }else{
+            System.out.println("1550");
+        }
+        
+        
+    }
+}
