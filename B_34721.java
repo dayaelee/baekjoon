7becdb84a605e4ab2f3f930a59f5a07f188d4d3f
dayaelee <dayae.dev@gmail.com>
2026-01-28 18:07:04 +0900

    [solved] baekjoon 34721 역사를 걸으면 동국이 보이고

---
+++ B_34721.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    public static int n, m;
+    public static int [] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        n = Integer.parseInt(str);
+        for(int i = 0; i<n; i++){
+            System.out.println("I love DGU");
+        }
+
+
+        
+    }
+    
+}
