e3214aba90f012801da262ba2ada1cde7d37ab30
dayaelee <dayae.dev@gmail.com>
2026-03-09 21:09:32 +0900

    [solved] baekjoon 2910 빈도 정렬

--- B_2910.java
+++ B_2910.java
@@ -2,68 +2,64 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static class message implements Comparable<message>{
+    static class Message implements Comparable<Message>{
         int key;
         int value;
-        int idx;
-        message(int key, int value, int idx){
+        Message(int key, int value){
             this.key = key;
             this.value = value;
-            this.idx = idx;
         }
 
         @Override
-        public int compareTo(message o){
-            if(value !=o.value)
-                return o.value - value;
-            return idx - o.idx;
+        public int compareTo(Message o){
+            return o.value-value;
         }
     }
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
+        String tmp = br.readLine();
 
-        StringTokenizer st = new StringTokenizer(str);
-
+        StringTokenizer st = new StringTokenizer(tmp);
         int n = Integer.parseInt(st.nextToken());
         int c = Integer.parseInt(st.nextToken());
 
-        String tmp = br.readLine();
+        tmp = br.readLine();
+
         String[] tmpArr = tmp.split(" ");
 
-        int[] numArr = Arrays.stream(tmpArr)
-                            .mapToInt(Integer::parseInt)
-                            .toArray();
+        int[] intArr= Arrays.stream(tmpArr).mapToInt(Integer::parseInt).toArray();
 
-        // 정수 배열 정렬
-        // Arrays.sort(numArr);
-
-        Map<Integer, int[]> map = new HashMap<>();
-        for(int i = 0; i<numArr.length; i++){
-            if (!map.containsKey(numArr[i])){
-                map.put(numArr[i], new int[]{1, i});
+        Map<Integer, int[]> map = new LinkedHashMap<>();
+        for(int i = 0; i<n; i++){
+            if(!map.containsKey(intArr[i])){
+                map.put(intArr[i], new int[]{1});
             }else{
-                map.get(numArr[i])[0]++;
+                map.get(intArr[i])[0]++; 
             }
         }
-        
 
-        List<message> list = new ArrayList<>();
-        for (int key : map.keySet()){
+        List<Message> list = new ArrayList<>();
+
+        for(Integer key: map.keySet()){
             int[] info = map.get(key);
-            list.add(new message(key, info[0], info[1]));
+
+            list.add(new Message(key, info[0]));
         }
 
         Collections.sort(list);
 
-
         StringBuilder sb = new StringBuilder();
-        for(message tmpMap: list){   
-            for(int i = 0; i<tmpMap.value; i++) 
-                sb.append(tmpMap.key+" ");
+
+        for(Message m: list){
+            for(int i = 0; i<m.value; i++){
+                sb.append(m.key+" ");
+            }
         }
-        
 
         System.out.println(sb.toString());
-    }    
+
+        
+    }
+    
 }
