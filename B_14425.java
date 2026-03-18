b789ce325ae1ab7000398a6238e915a066d7bf9d
dayaelee <dayae.dev@gmail.com>
2026-03-18 22:04:18 +0900

    [solved] baekjoon 문자열 집합

--- B_14425.java
+++ B_14425.java
@@ -2,6 +2,23 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
+    static boolean isExist(String[] tmp, int n, String s){
+        int start = 0;
+        int end = n-1;
+        while(start<=end){
+            int mid = (start+end)/2;
+            if(tmp[mid].equals(s)){
+                return true;
+            } else if (tmp[mid].compareTo(s)<0){
+                start=mid+1;
+            } else if (tmp[mid].compareTo(s)>=0){
+                end = mid-1;
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) throws IOException  {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -12,16 +29,15 @@ public class Main {
         int m = Integer.parseInt(st.nextToken());    
 
 
-        Set<String> tmp = new HashSet<>();
+        String[] tmp = new String[n];
 
-        for(int i = 0; i<n; i++){
-            tmp.add(br.readLine());
+        for(int i = 0; i < n; i++){
+            tmp[i] = br.readLine();
         }
-
-
+        Arrays.sort(tmp);
         int cnt=0;
         for(int i = 0; i<m; i++){
-            if(tmp.contains(br.readLine()))
+            if(isExist(tmp, n, br.readLine()))
                 cnt+=1;
         }
 
