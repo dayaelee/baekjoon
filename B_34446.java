a597af70b4efd7e6c36ab52583025fdec74e07bd
dayaelee <dayae.dev@gmail.com>
2026-03-20 22:49:29 +0900

    [solved] baekjoon 34446 E-Days Ore Cart Pull

---
+++ B_34446.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+
+
+
+    public static void main(String[] args) throws IOException   {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String str = br.readLine();
+        int n = Integer.parseInt(str);
+
+        str = br.readLine();
+        int n2 = Integer.parseInt(str);
+
+        str = br.readLine();
+        int n3 = Integer.parseInt(str);
+
+
+        System.out.println(n+n);
+
+
+    }
+    
+}
