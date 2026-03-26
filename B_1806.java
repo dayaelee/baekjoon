73c6a34d299bb107054eead001f1d79c5455573e
dayaelee <dayae.dev@gmail.com>
2026-03-26 19:13:31 +0900

    [solved] baekjoon 1806 부분합

---
+++ B_1806.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 이 부분을 추가하면 콘솔 입력 대신 input.txt 파일을 읽습니다.
+        // 제출할 때는 이 줄만 주석 처리하거나 지우면 됩니다!
+        System.setIn(new FileInputStream("input.txt"));
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+
+        StringTokenizer st = new StringTokenizer(str);
+        int n = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+
+
+        str = br.readLine();
+        st = new StringTokenizer(str);
+        for(int i = 0; i<n; i++){
+            arr[i]=Integer.parseInt(st.nextToken());
+        }
+
+        int l = 0, r = 0;
+        long sum = 0, ans = Long.MAX_VALUE;
+
+        while(true){
+
+            if(sum>=s){
+                sum-=arr[l++];
+            } else if (r>=n){
+                break;
+            } else{
+                sum+=arr[r++];
+            }
+
+            // System.out.println("l: "+l+" r: "+r+" sum: "+sum+" ans: "+ (r-l+1));
+
+            if(sum >= s){
+                ans = Math.min(ans, r-1-l+1);
+            }
+        }
+
+        if (ans == Long.MAX_VALUE){
+            ans = 0;
+        }
+
+        System.out.println(ans);
+
+
+
+
+
+
+    }
+    
+}
