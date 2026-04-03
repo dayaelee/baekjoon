cafaff007a5e5eeda7952c696e180f434e3c9ddf
dayaelee <dayae.dev@gmail.com>
2026-04-03 18:53:39 +0900

    [solved] baekjoon 34443 Hack-a-Holics

---
+++ B_34443.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 이 부분을 추가하면 콘솔 입력 대신 input.txt 파일을 읽습니다.
+        // 제출할 때는 이 줄만 주석 처리하거나 지우면 됩니다!
+        System.setIn(new FileInputStream("input.txt"));
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        int n = Integer.parseInt(str);
+        str = br.readLine();
+
+        int c = Integer.parseInt(str);
+        str = br.readLine();
+        int p = Integer.parseInt(str);
+
+        System.out.println((n*p));
+        
+        
+    }
+    
+}
