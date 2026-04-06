fdf9d7362be02f3861fea2c1456e1d28d7ff2d2c
dayaelee <dayae.dev@gmail.com>
2026-04-06 17:47:39 +0900

    [solved] baekjoon 26307 Correct

---
+++ B_26307.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 이 부분을 추가하면 콘솔 입력 대신 input.txt 파일을 읽습니다.
+        // 제출할 때는 이 줄만 주석 처리하거나 지우면 됩니다!
+        System.setIn(new FileInputStream("input.txt"));
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int h = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+
+        int hh=21, mm=0;
+
+        
+
+        int ah = (h+12)-hh;
+        int am = m;
+        System.out.println((ah*60)+am);
+
+
+        
+        
+    }
+    
+}
