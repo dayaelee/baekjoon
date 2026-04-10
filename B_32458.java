63c6beaaeced1ef58570cda0df8e0f2558b0f853
dayaelee <dayae.dev@gmail.com>
2026-04-11 08:58:13 +0900

    [solved] baekjoon 32458 Just Round Down

---
+++ B_32458.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 이 부분을 추가하면 콘솔 입력 대신 input.txt 파일을 읽습니다.
+        // 제출할 때는 이 줄만 주석 처리하거나 지우면 됩니다!
+        System.setIn(new FileInputStream("input.txt"));
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        double m = Double.parseDouble(br.readLine());
+        System.out.println((int)m);
+        
+    }
+    
+}
